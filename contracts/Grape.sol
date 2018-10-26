ac2fd5d0b1aa0e62e5692773f3c1c557b14ee6a6
SayBGM <bgm537@saybgm.me>
2018-10-26 20:51:33 +0900

    getConcert, createConcert 부분 구조 수정

--- contracts/Grape.sol
+++ contracts/Grape.sol
@@ -1,27 +1,22 @@
 pragma solidity ^0.4.23;
-
+pragma experimental ABIEncoderV2;
 
 contract Grape {
     uint256 concertID = 0;
-    uint256 ticketID = 0;
-
-    struct TicketType {
-        string ticketName;
-        uint256 ticketPrice;
-        string ticketDescription;
-        uint256 ticketAmount;
-    }
 
     struct Concert {
         uint256 concertId;
         address creator;
         string name;
-        string place;
         string term;
         string ownerName;
         string ticketTypes;
         bool isPossibleAuction;
-        string tags;
+        uint256 ticketTypeCounts;
+        string[] ticketName;
+        uint256[] ticketPrice;
+        string[] ticketDescription;
+        uint256[] ticketAmount;
         uint256 ticketListCount;
         mapping (uint256 => Ticket) ticketList;
     }
@@ -36,36 +31,52 @@ contract Grape {
         bool isAuction;
     }
 
-    function createConcert(string name, string place, string term, string ownerName, string ticketTypes, bool isPossibleAuction, string tags) 
+    function createConcert(string _name, string _term, string _ownerName, string _ticketTypes, bool _isPossibleAuction,
+    uint256 _ticketTypeCounts, string[] _ticketName, uint256[] _ticketPrice, string[] _ticketDescription, uint256[] _ticketAmount) 
     public payable returns(uint256 concertID){
-        concertList[concertID] = (Concert(concertID, msg.sender, name, place, term, ownerName, ticketTypes, isPossibleAuction, tags, 0));
+        concertList[concertID] = (Concert(concertID, msg.sender, _name, _term, _ownerName, _ticketTypes, _isPossibleAuction, _ticketTypeCounts, _ticketName, _ticketPrice, _ticketDescription, _ticketAmount, 0));
         concertID = concertID++;
     }
     function getConcert(uint256 concertId) public view returns (
-        address creator,
-        string name,
-        string place,
-        string term,
-        string ownerName,
-        string ticketTypes,
-        bool isPossibleAuction,
-        string tags,
-        uint256 ticketListCount
+        address _creator,
+        string _name,
+        string _term,
+        string _ownerName,
+        string _ticketTypes,
+        uint256 _ticketTypeCounts,
+        string[] _ticketName,
+        uint256[] _ticketPrice,
+        string[] _ticketDescription,
+        uint256[] _ticketAmount,
+        bool _isPossibleAuction,
+        uint256 _ticketListCount
     )
     {
-        creator = concertList[concertId].creator;
-        name = concertList[concertId].name;
-        place = concertList[concertId].place;
-        term = concertList[concertId].term;
-        ownerName = concertList[concertId].ownerName;
-        ticketTypes = concertList[concertId].ticketTypes;
-        isPossibleAuction = concertList[concertId].isPossibleAuction;
-        tags = concertList[concertId].tags;
-        ticketListCount = concertList[concertId].ticketListCount;
+        _creator = concertList[concertId].creator;
+        _name = concertList[concertId].name;
+        _term = concertList[concertId].term;
+        _ownerName = concertList[concertId].ownerName;
+        _ticketTypes = concertList[concertId].ticketTypes;
+        _isPossibleAuction = concertList[concertId].isPossibleAuction;
+        _ticketListCount = concertList[concertId].ticketListCount;
+        _ticketName = concertList[concertId]._ticketName;
+        _ticketPrice = concertList[concertId]._ticketPrice;
+        _ticketDescription = concertList[concertId]._ticketDescription;
+        _ticketAmount = concertList[concertId]._ticketAmount;
     }
+
     function getconcertID() returns (
-        uint256 concertID
+        uint256 _concertID
     ) {
-        concertID = concertID;
+        _concertID = concertID;
+    }
+
+    function payTicekt(uint256 concertId, uint256 ticketPrice, string ticketName, string date) public payable {
+        concertList[concertId].creator.send(ticketPrice);
+        concertList[concertId].ticketList[concertList[concertId].ticketListCount].concertId = concertId;
+        concertList[concertId].ticketList[concertList[concertId].ticketListCount].owner = msg.sender;
+        concertList[concertId].ticketList[concertList[concertId].ticketListCount].date = date;
+        concertList[concertId].ticketList[concertList[concertId].ticketListCount].isAuction = false;
+        concertList[concertId].ticketListCount = concertList[concertId].ticketListCount++;
     }
 }
--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -1,7 +1,7 @@
 import Caver from 'caver-js'
 
 export const config = {
-  rpcURL: 'http://localhost:8551'
+  rpcURL: 'http://localhost:8551',
 }
 
 export const cav = new Caver(config.rpcURL)
