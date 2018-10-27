9945da725748901ad1562d4160bbbfc70de4f243
SayBGM <bgm537@saybgm.me>
2018-10-27 13:26:38 +0900

    buyUsedTicket, sellUsedTicket 함수 구현

--- contracts/Grape.sol
+++ contracts/Grape.sol
@@ -39,13 +39,22 @@ contract Grape {
         address owner;
         uint256 ticketType;
         uint256 date;
-        string auctionHistorys;
+        uint256 ticketHistoryCount;
+        mapping (uint256 => AuctionHistory) auctionHistorys;
         bool isAuction;
+    }
+    
+    struct AuctionHistory {
+        uint256 date;
+        address owner;
+        uint256 price;
     }
 
     function createConcert(bool _isPossibleAuction,
     uint256 _ticketTypeCounts, string[] _ticketName, uint256[] _ticketPrice, string[] _ticketDescription, uint256[] _ticketAmount) 
     public payable returns(uint256 concertID){
+        address foundation = 0xC39b1048d6DD7fB500A2E8F9fFFa0ca33cd4dB5a;
+        foundation.transfer(20);
         concertList[concertID] = (Concert(concertID, msg.sender, _isPossibleAuction, _ticketTypeCounts, _ticketName, _ticketPrice, _ticketDescription, _ticketAmount, 0));
         concertID++;
     }
@@ -84,6 +93,42 @@ contract Grape {
         concertList[_concertId].ticketList[concertList[_concertId].ticketListCount].ticketType = _ticketType;
         concertList[_concertId].ticketList[concertList[_concertId].ticketListCount].date = block.timestamp;
         concertList[_concertId].ticketList[concertList[_concertId].ticketListCount].isAuction = false;
+        concertList[_concertId].ticketList[concertList[_concertId].ticketListCount].ticketHistoryCount = 0;
         concertList[_concertId].ticketListCount++;
+    }
+
+    function buyUsedTicket(uint256 _concertId, uint256 _ticketType) public payable {
+        for (uint256 i = 0; i < concertList[_concertId].ticketListCount; i++) {
+            if (concertList[_concertId].ticketList[i].ticketType == _ticketType && concertList[_concertId].ticketList[i].isAuction) {
+                concertList[_concertId].ticketList[i].owner.transfer(concertList[_concertId].ticketPrice[_ticketType]);
+                concertList[_concertId].creator.transfer(concertList[_concertId].ticketPrice[_ticketType] * 1 / 10);
+                concertList[_concertId].ticketList[i].isAuction = false;
+                concertList[_concertId].ticketList[i].auctionHistorys[concertList[_concertId].ticketList[i].ticketHistoryCount].date = block.timestamp;
+                concertList[_concertId].ticketList[i].auctionHistorys[concertList[_concertId].ticketList[i].ticketHistoryCount].owner = concertList[_concertId].ticketList[i].owner;
+                concertList[_concertId].ticketList[i].auctionHistorys[concertList[_concertId].ticketList[i].ticketHistoryCount].price = concertList[_concertId].ticketPrice[_ticketType] * 11 / 10;
+                concertList[_concertId].ticketList[i].ticketHistoryCount++;
+                concertList[_concertId].ticketList[i].owner = msg.sender;
+                break;
+            }
+        }
+    }
+    function sellUsedTicket(uint256 _concertId, uint256 _ticketType) public payable {
+        for (uint256 i = 0; i < concertList[_concertId].ticketListCount; i++) {
+            if (concertList[_concertId].ticketList[i].owner == msg.sender && !concertList[_concertId].ticketList[i].isAuction) {
+                concertList[_concertId].creator.transfer(concertList[_concertId].ticketPrice[_ticketType] * 1 / 10);
+                concertList[_concertId].ticketList[i].isAuction = true;
+                break;
+            }
+        }
+    }
+
+    function cancleSellUsedTicket(uint256 _concertId, uint256 _ticketType) public payable {
+        for (uint256 i = 0; i < concertList[_concertId].ticketListCount; i++) {
+            if (concertList[_concertId].ticketList[i].owner == msg.sender && !concertList[_concertId].ticketList[i].isAuction) {
+                concertList[_concertId].creator.transfer(concertList[_concertId].ticketPrice[_ticketType] * 1 / 10);
+                concertList[_concertId].ticketList[i].isAuction = false;
+                break;
+            }
+        }
     }
 }
